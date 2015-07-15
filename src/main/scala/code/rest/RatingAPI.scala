e4d8aace0525d7cd8ef0c95a5a94fb54f0f947a0
bacnv <bacnguyen87@gmail.com>
2015-07-15 19:50:56 +0700

    RatingAPI

--- src/main/scala/code/rest/RatingAPI.scala
+++ src/main/scala/code/rest/RatingAPI.scala
@@ -44,7 +44,7 @@ object RatingAPI extends RestHelper {
 
 
       case "rating" :: "insert" :: Nil JsonPost json-> request => insert(json)
-      case "rating" :: "update" :: Nil JsonPost json-> request => update(json)
+      case "rating" :: "update" :: Nil JsonPost json-> request => updates(json)
       case "rating" :: "delete" :: Nil JsonPost json-> request => delete(json)
       case "rating" :: "getmodelid" :: q :: Nil JsonGet req => getbymodelid(q)
 
@@ -195,12 +195,72 @@ if (list.isInstanceOf[JArray]) {
 
       c = d.asJValue
     }else{
-
+     c= updates(q)
 
     }
 
 
     c
+  }
+
+  def updates(q:JValue): JValue = {
+    var json = (q\"codein").asInstanceOf[JObject]
+    var msg : JValue ={"ERROR"->"Not found"} :JValue
+
+    //    var listFactorOption : List[codeIN] = List()
+    var listi  : List[codeIN] = List()
+    var listu  : List[codeIN] = List()
+
+    val qry = QueryBuilder.start("modelid").is(q.asInstanceOf[JObject].values.apply("modelid")).get
+
+    var dbFind = Rating.findAll(qry)
+
+//    val code = for { JField("code", JString(code)) <- q } yield code
+
+    //    val qry1 = QueryBuilder.start("_id").is(json.values.apply("_id")).put("rate.code").is(code).get
+
+    //    dbFind = Rating.findAll("$where" -> "function() { return this.rate.status=='AAA'}")
+
+
+
+    val factorOption = codeIN.code(json.values.apply("code").toString).status(json.values.apply("status").toString)
+      .statusname(json.values.apply("statusname").toString).scorefrom(json.values.apply("scorefrom").toString.toDouble)
+      .scoreto(json.values.apply("scoreto").toString.toDouble)
+
+
+
+    //    listFactorOption =  dbFind(0).codein.value
+    if(dbFind.size > 0) {
+
+      val JArray(rates) = (dbFind.map(_.asJValue) \ "codein")
+      rates collect { case rate: JObject => rate } foreach getlist
+
+      def getlist(rate: JObject) = {
+        var listb: List[codeIN] = List()
+        val j = rate.asInstanceOf[JObject].values
+        if (j.apply("code").toString.equals(json.values.apply("code").toString)) {
+
+        } else {
+          val item = codeIN.createRecord.code(j.apply("code").toString).status(j.apply("status").toString)
+            .statusname(j.apply("statusname").toString).scorefrom(j.apply("scorefrom").toString.toDouble)
+            .scoreto(j.apply("scoreto").toString.toDouble)
+          listb = List(item)
+          listi = listi ::: listb
+        }
+
+
+      }
+
+
+      val updateFactor = dbFind(0).update.codein(listi ::: listu ::: List(factorOption)).save
+
+      msg = {"SUCCESS" -> dbFind.map(_.asJValue)} :JValue
+
+    }else{
+      msg = {"ERROR" -> "Cannot find model"} :JValue
+    }
+
+    msg
   }
 
   def update(q:JValue): JValue = {
