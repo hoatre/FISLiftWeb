6436160c61f209d68eb475dc6d395b4c33c5cc57
phonghh2 <phonghh1989@gmail.com>
2015-07-17 14:58:18 +0700

    push

--- src/main/scala/code/rest/RatingAPI.scala
+++ src/main/scala/code/rest/RatingAPI.scala
@@ -22,60 +22,69 @@ object RatingAPI extends RestHelper {
   }
 
 
-    serve {
+  serve {
 
 
+    case "rating" :: "insert" :: Nil JsonPost json -> request => insert(json)
+    case "rating" :: "update" :: Nil JsonPost json -> request => updates(json)
+    case "rating" :: "delete" :: Nil JsonPost json -> request => delete(json)
+    case "rating" :: "getmodelid" :: q :: Nil JsonGet req => getbymodelid(q)
 
-      case "rating" :: "insert" :: Nil JsonPost json-> request => insert(json)
-      case "rating" :: "update" :: Nil JsonPost json-> request => updates(json)
-      case "rating" :: "delete" :: Nil JsonPost json-> request => delete(json)
-      case "rating" :: "getmodelid" :: q :: Nil JsonGet req => getbymodelid(q)
+    case "rating" :: "getcode" :: q :: p :: Nil JsonGet req => getbycodelid(q, p)
+    case "rating" :: "getall" :: Nil JsonGet req => getall
 
-      case "rating" :: "getcode" :: q :: p:: Nil JsonGet req => getbycodelid(q,p)
-      case "rating" :: "getall" :: Nil JsonGet req => getall
+    case "rating" :: "insert" :: Nil Options _ => {
+      "OK" -> "200"
+    }: JValue
+    case "rating" :: "update" :: Nil Options _ => {
+      "OK" -> "200"
+    }: JValue
+    case "rating" :: "delete" :: Nil Options _ => {
+      "OK" -> "200"
+    }: JValue
 
-      case "rating" :: "insert" :: Nil Options _ => {"OK" -> "200"} :JValue
-      case "rating" :: "update" :: Nil Options _ => {"OK" -> "200"} :JValue
-      case "rating" :: "delete" :: Nil Options _ => {"OK" -> "200"} :JValue
 
+  }
 
-
-
-    }
-
-  def getbymodelid(q:String): JValue={
+  def getbymodelid(q: String): JValue = {
     val qry = QueryBuilder.start("modelid").is(q).get
 
     val db = Rating.findAll(qry)
 
-    var msg : JValue= {"ERROR" -> "Not existed"} :JValue
+    var msg: JValue = {
+      "ERROR" -> "Not existed"
+    }: JValue
 
-    if(db.size>0) {
+    if (db.size > 0) {
 
-    msg = { "SUCCESS" -> db.map(_.asJValue): JValue}:JValue
+      msg = {
+        "SUCCESS" -> db.map(_.asJValue): JValue
+      }: JValue
 
     }
 
     msg
   }
 
-  def getbycodelid(q:String,p:String): JValue={
+  def getbycodelid(q: String, p: String): JValue = {
     val qry = QueryBuilder.start("modelid").is(q).put("codein").elemMatch(new BasicDBObject("code", p)).get
 
     val db = Rating.findAll(qry)
 
-    var msg : JValue= {"ERROR" -> "Not existed"} :JValue
+    var msg: JValue = {
+      "ERROR" -> "Not existed"
+    }: JValue
 
-    if(db.size>0) {
+    if (db.size > 0) {
 
       val db1 = db(0).asJValue
-      val json = (db1\"codein")
+      val json = (db1 \ "codein")
 
       val JArray(rates) = json
       rates collect { case rate: JObject => rate } foreach myOperation
       def myOperation(rate: JObject) = {
         val j = rate.asInstanceOf[JObject].values
-        if(j.apply("code").toString.equals(p)) {
+        if (j.apply("code").toString.equals(p)) {
 
           val item = codeIN.code(j.apply("code").toString).status(j.apply("status").toString)
             .statusname(j.apply("statusname").toString).scorefrom(j.apply("scorefrom").toString.toDouble).scoreto(j.apply("scoreto").toString.toDouble)
@@ -93,20 +102,23 @@ object RatingAPI extends RestHelper {
       }
 
 
-
-
     }
 
     msg
   }
-  def getall :JValue={
+
+  def getall: JValue = {
     val db = Rating.findAll
 
-    var msg : JValue= {"ERROR" -> "Not existed"} :JValue
+    var msg: JValue = {
+      "ERROR" -> "Not existed"
+    }: JValue
 
-    if(db.size>0) {
+    if (db.size > 0) {
 
-      msg = { "SUCCESS" -> db.map(_.asJValue): JValue}:JValue
+      msg = {
+        "SUCCESS" -> db.map(_.asJValue): JValue
+      }: JValue
 
     }
 
@@ -117,79 +129,80 @@ object RatingAPI extends RestHelper {
   def insert(q: JValue): JValue = {
 
 
-
     var json = q.asInstanceOf[JObject]
 
     val qryM = QueryBuilder.start("_id").is(json.values.apply("modelid").toString())
       .get
     val DBM = ModelInfo.findAll(qryM)
-    if(DBM.equals("publish") || DBM.equals("active")){
+    if (DBM.equals("publish") || DBM.equals("active")) {
       {
         "ERROR" -> "Factor can't delete (model was published)"
       }: JValue
     }
 
     val modelid = json.values.apply("modelid").toString()
-//    val rate = json.values.apply("rate").toString.toList
-//    val a1 = codeIN.createRecord.code(json.values.apply("rate").apply("code").toString)
-//
-//    val a2 = codeIN.createRecord.code(json.values.apply("code").toString)
+    //    val rate = json.values.apply("rate").toString.toList
+    //    val a1 = codeIN.createRecord.code(json.values.apply("rate").apply("code").toString)
+    //
+    //    val a2 = codeIN.createRecord.code(json.values.apply("code").toString)
 
-//    val b :List(Code)
-//    println(rate)
-val qry = QueryBuilder.start("modelid").is(modelid).get
+    //    val b :List(Code)
+    //    println(rate)
+    val qry = QueryBuilder.start("modelid").is(modelid).get
 
     val DBquery = Rating.findAll(qry)
-    var c : JValue = {"ERROR" -> "Check again"} :JValue
+    var c: JValue = {
+      "ERROR" -> "Check again"
+    }: JValue
 
-    if(DBquery.size ==0){
-    var i = 0
-    var lista : List[codeIN] =List()
-    val list = (json \ "codein")
-
-
-if (list.isInstanceOf[JArray]) {
-  val JArray(rates) = list
-  rates collect { case rate: JObject => rate } foreach myOperation
-  def myOperation(rate: JObject) = {
-    val j = rate.asInstanceOf[JObject].values
-
-    val item = codeIN.createRecord.code(j.apply("code").toString).status(j.apply("status").toString)
-      .statusname(j.apply("statusname").toString).scorefrom(j.apply("scorefrom").toString.toDouble).scoreto(j.apply("scoreto").toString.toDouble)
-
-    var listb: List[codeIN] = List(item)
-
-    lista = lista ::: listb
-  }
-
-}else{
-  val j = list.children
-  for{
-    JString(code) <- (j \\ "code").toOpt
-    JString(status) <- (j \\ "status").toOpt
-    JString(statusname) <- (j \\ "statusname").toOpt
-    JString(scorefrom) <- (j \\ "scorefrom").toOpt
-    JString(scoreto) <- (j \\ "scoreto").toOpt
-
-    item = codeIN.createRecord.code(code.toString).status(status.toString).statusname(statusname.toString).scorefrom(scorefrom.toString.toDouble).scoreto(scoreto.toString.toDouble)
+    if (DBquery.size == 0) {
+      var i = 0
+      var lista: List[codeIN] = List()
+      val list = (json \ "codein")
 
 
-  }yield {
+      if (list.isInstanceOf[JArray]) {
+        val JArray(rates) = list
+        rates collect { case rate: JObject => rate } foreach myOperation
+        def myOperation(rate: JObject) = {
+          val j = rate.asInstanceOf[JObject].values
 
-    var listb: List[codeIN] = List(item)
+          val item = codeIN.createRecord.code(j.apply("code").toString).status(j.apply("status").toString)
+            .statusname(j.apply("statusname").toString).scorefrom(j.apply("scorefrom").toString.toDouble).scoreto(j.apply("scoreto").toString.toDouble)
 
-    lista = lista ::: listb
-  }
-//  val item = codeIN.createRecord.code(j.apply("code").toString).status(j.apply("status").toString)
-}
+          var listb: List[codeIN] = List(item)
+
+          lista = lista ::: listb
+        }
+
+      } else {
+        val j = list.children
+        for {
+          JString(code) <- (j \\ "code").toOpt
+          JString(status) <- (j \\ "status").toOpt
+          JString(statusname) <- (j \\ "statusname").toOpt
+          JString(scorefrom) <- (j \\ "scorefrom").toOpt
+          JString(scoreto) <- (j \\ "scoreto").toOpt
+
+          item = codeIN.createRecord.code(code.toString).status(status.toString).statusname(statusname.toString).scorefrom(scorefrom.toString.toDouble).scoreto(scoreto.toString.toDouble)
+
+
+        } yield {
+
+          var listb: List[codeIN] = List(item)
+
+          lista = lista ::: listb
+        }
+        //  val item = codeIN.createRecord.code(j.apply("code").toString).status(j.apply("status").toString)
+      }
 
 
 
       val d = Rating.createRecord.id(UUID.randomUUID().toString()).modelid(modelid).codein(lista).save
 
       c = d.asJValue
-    }else{
-     c= updates(q)
+    } else {
+      c = updates(q)
 
     }
 
@@ -197,17 +210,19 @@ if (list.isInstanceOf[JArray]) {
     c
   }
 
-  def updates(q:JValue): JValue = {
-    var json = (q\"codein").asInstanceOf[JObject]
-    var msg : JValue ={"ERROR"->"Not found"} :JValue
+  def updates(q: JValue): JValue = {
+    var json = (q \ "codein").asInstanceOf[JObject]
+    var msg: JValue = {
+      "ERROR" -> "Not found"
+    }: JValue
 
     //    var listFactorOption : List[codeIN] = List()
-    var listi  : List[codeIN] = List()
-    var listu  : List[codeIN] = List()
+    var listi: List[codeIN] = List()
+    var listu: List[codeIN] = List()
     val qryM = QueryBuilder.start("_id").is(q.asInstanceOf[JObject].values.apply("modelid").toString)
       .get
     val DBM = ModelInfo.findAll(qryM)
-    if(DBM.equals("publish") || DBM.equals("active")){
+    if (DBM.equals("publish") || DBM.equals("active")) {
       {
         "ERROR" -> "Factor can't delete (model was published)"
       }: JValue
@@ -216,12 +231,11 @@ if (list.isInstanceOf[JArray]) {
 
     var dbFind = Rating.findAll(qry)
 
-//    val code = for { JField("code", JString(code)) <- q } yield code
+    //    val code = for { JField("code", JString(code)) <- q } yield code
 
     //    val qry1 = QueryBuilder.start("_id").is(json.values.apply("_id")).put("rate.code").is(code).get
 
     //    dbFind = Rating.findAll("$where" -> "function() { return this.rate.status=='AAA'}")
-
 
 
     val factorOption = codeIN.code(json.values.apply("code").toString).status(json.values.apply("status").toString)
@@ -231,7 +245,7 @@ if (list.isInstanceOf[JArray]) {
 
 
     //    listFactorOption =  dbFind(0).codein.value
-    if(dbFind.size > 0) {
+    if (dbFind.size > 0) {
 
       val JArray(rates) = (dbFind.map(_.asJValue) \ "codein")
       rates collect { case rate: JObject => rate } foreach getlist
@@ -255,27 +269,50 @@ if (list.isInstanceOf[JArray]) {
 
       val updateFactor = dbFind(0).update.codein(listi ::: listu ::: List(factorOption)).save
 
-      msg = {"SUCCESS" -> dbFind.map(_.asJValue)} :JValue
+      msg = {
+        "SUCCESS" -> dbFind.map(_.asJValue)
+      }: JValue
 
-    }else{
-      msg = {"ERROR" -> "Cannot find model"} :JValue
+    } else {
+      msg = {
+        "ERROR" -> "Cannot find model"
+      }: JValue
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     msg
   }
 
-  def update(q:JValue): JValue = {
+  def update(q: JValue): JValue = {
     var json = q.asInstanceOf[JObject]
-    var msg : JValue ={"ERROR"->"Not found"} :JValue
+    var msg: JValue = {
+      "ERROR" -> "Not found"
+    }: JValue
 
-//    var listFactorOption : List[codeIN] = List()
-    var listi  : List[codeIN] = List()
-    var listu  : List[codeIN] = List()
+    //    var listFactorOption : List[codeIN] = List()
+    var listi: List[codeIN] = List()
+    var listu: List[codeIN] = List()
 
     val qryM = QueryBuilder.start("_id").is(json.values.apply("modelid").toString)
       .get
     val DBM = ModelInfo.findAll(qryM)
-    if(DBM.equals("publish") || DBM.equals("active")){
+    if (DBM.equals("publish") || DBM.equals("active")) {
       {
         "ERROR" -> "Factor can't delete (model was published)"
       }: JValue
@@ -285,12 +322,11 @@ if (list.isInstanceOf[JArray]) {
 
     var dbFind = Rating.findAll(qry)
 
-   val code = for { JField("code", JString(code)) <- q } yield code
+    val code = for {JField("code", JString(code)) <- q} yield code
 
-//    val qry1 = QueryBuilder.start("_id").is(json.values.apply("_id")).put("rate.code").is(code).get
+    //    val qry1 = QueryBuilder.start("_id").is(json.values.apply("_id")).put("rate.code").is(code).get
 
-//    dbFind = Rating.findAll("$where" -> "function() { return this.rate.status=='AAA'}")
-
+    //    dbFind = Rating.findAll("$where" -> "function() { return this.rate.status=='AAA'}")
 
 
     val factorOption = codeIN.code(json.values.apply("code").toString).status(json.values.apply("status").toString)
@@ -299,8 +335,8 @@ if (list.isInstanceOf[JArray]) {
 
 
 
-//    listFactorOption =  dbFind(0).codein.value
-    if(dbFind.size > 0) {
+    //    listFactorOption =  dbFind(0).codein.value
+    if (dbFind.size > 0) {
 
       val JArray(rates) = (dbFind.map(_.asJValue) \ "codein")
       rates collect { case rate: JObject => rate } foreach getlist
@@ -324,52 +360,55 @@ if (list.isInstanceOf[JArray]) {
 
       val updateFactor = dbFind(0).update.codein(listi ::: listu ::: List(factorOption)).save
 
-     msg = {"SUCCESS" -> dbFind.map(_.asJValue)} :JValue
+      msg = {
+        "SUCCESS" -> dbFind.map(_.asJValue)
+      }: JValue
 
-    }else{
-      msg = {"ERROR" -> "Cannot find model"} :JValue
+    } else {
+      msg = {
+        "ERROR" -> "Cannot find model"
+      }: JValue
     }
 
     msg
   }
 
 
-
-  def delete(q:JValue): JValue = {
+  def delete(q: JValue): JValue = {
     var json = q.asInstanceOf[JObject]
 
     val qryM = QueryBuilder.start("_id").is(json.values.apply("modelid").toString)
       .get
     val DBM = ModelInfo.findAll(qryM)
-    if(DBM.equals("publish") || DBM.equals("active")){
+    if (DBM.equals("publish") || DBM.equals("active")) {
       {
         "ERROR" -> "Factor can't delete (model was published)"
       }: JValue
     }
-//    var listFactorOption : List[codeIN] = List()
-    var listi  : List[codeIN] = List()
-    var listu  : List[codeIN] = List()
+    //    var listFactorOption : List[codeIN] = List()
+    var listi: List[codeIN] = List()
+    var listu: List[codeIN] = List()
 
     val qry = QueryBuilder.start("modelid").is(json.values.apply("modelid")).get
 
     var dbFind = Rating.findAll(qry)
 
-//    val code = for { JField("code", JString(code)) <- q } yield code
+    //    val code = for { JField("code", JString(code)) <- q } yield code
 
     //    val qry1 = QueryBuilder.start("_id").is(json.values.apply("_id")).put("rate.code").is(code).get
 
     //    dbFind = Rating.findAll("$where" -> "function() { return this.rate.status=='AAA'}")
 
 
-//    listFactorOption =  dbFind(0).codein.value
+    //    listFactorOption =  dbFind(0).codein.value
 
-    val JArray(rates) =   (dbFind.map(_.asJValue) \ "codein")
+    val JArray(rates) = (dbFind.map(_.asJValue) \ "codein")
     rates collect { case rate: JObject => rate } foreach getlist
 
     def getlist(rate: JObject) = {
       var listb: List[codeIN] = List()
       val j = rate.asInstanceOf[JObject].values
-      if(!j.apply("code").toString.equals(json.values.apply("code").toString)){
+      if (!j.apply("code").toString.equals(json.values.apply("code").toString)) {
         val item = codeIN.createRecord.code(j.apply("code").toString).status(j.apply("status").toString)
           .statusname(j.apply("statusname").toString).scorefrom(j.apply("scorefrom").toString.toDouble)
           .scoreto(j.apply("scoreto").toString.toDouble)
@@ -378,15 +417,14 @@ if (list.isInstanceOf[JArray]) {
       }
 
 
-
-
     }
 
 
     val updateFactor = dbFind(0).update.codein(listi ::: listu).save
 
 
-
-    {"SUCCESS" -> dbFind.map(_.asJValue)} :JValue
+    {
+      "SUCCESS" -> dbFind.map(_.asJValue)
+    }: JValue
   }
 }
