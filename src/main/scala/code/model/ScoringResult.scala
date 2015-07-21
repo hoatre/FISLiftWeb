59b7995a033c72c26e80b7696bfc29db2068caa2
bacnv <bacnguyen87@gmail.com>
2015-07-21 11:39:16 +0700

    ScoreResultAPI

--- src/main/scala/code/model/ScoringResult.scala
+++ src/main/scala/code/model/ScoringResult.scala
@@ -2,13 +2,13 @@ package code.model
 
 import net.liftweb.common.Full
 import net.liftweb.mongodb.record.{MongoRecord, BsonMetaRecord, BsonRecord, MongoMetaRecord}
-import net.liftweb.mongodb.record.field.{StringPk, BsonRecordListField, StringRefField}
+import net.liftweb.mongodb.record.field.{ObjectIdPk, StringPk, BsonRecordListField, StringRefField}
 import net.liftweb.record.field.{LongField, DateTimeField, DoubleField, StringField}
 
 /**
  * Created by Administrator on 7/15/2015.
  */
-class ScoringResult private () extends MongoRecord[ScoringResult] with StringPk[ScoringResult] {
+class ScoringResult private () extends MongoRecord[ScoringResult] with ObjectIdPk[ScoringResult] {
 
   override def meta = ScoringResult
 
@@ -16,11 +16,12 @@ class ScoringResult private () extends MongoRecord[ScoringResult] with StringPk[
   object modelid extends StringRefField(this, ModelInfo, 512){
     override def options = ModelInfo.findAll.map(rd => (Full(rd.id.is), rd.name.is) )
   }
-  object Timestamp extends LongField(this)
-  object Scoring extends DoubleField(this)
-  object RatingCode extends StringField(this, 1024)
-  object RatingStatus extends StringField(this, 1024)
-  object ResultIN extends BsonRecordListField(this,resultIN)
+  object time_stamp extends LongField(this)
+  object scoring extends DoubleField(this)
+  object rating_code extends StringField(this, 1024)
+  object rating_status extends StringField(this, 1024)
+  object resulin extends BsonRecordListField(this,resultIN)
+  object factor extends BsonRecordListField(this,Factor)
 }
 
 object ScoringResult extends ScoringResult with MongoMetaRecord[ScoringResult] {
@@ -29,13 +30,13 @@ object ScoringResult extends ScoringResult with MongoMetaRecord[ScoringResult] {
 
 class resultIN private () extends BsonRecord[resultIN] {
   def meta = resultIN
-  object PathFactor extends BsonRecordListField(this, FactorPath)
-  object FactorId extends StringRefField(this, Factor, 512){
+  object path_factor extends BsonRecordListField(this, FactorPath)
+  object factor_id extends StringRefField(this, Factor, 512){
     override def options = Factor.findAll.map(rd => (Full(rd.id.is), rd.FactorName.is) )
   }
-  object FactorOptionId extends StringField(this, 1024)
-  object FactorOptionName extends StringField(this, 1024)
-  object FactorOptionScore extends DoubleField(this, 10)
+  object factor_option_id extends StringField(this, 1024)
+  object factor_name extends StringField(this, 1024)
+  object factor_option_score extends DoubleField(this, 10)
 }
 
 object resultIN extends resultIN with BsonMetaRecord[resultIN]
