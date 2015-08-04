5faab90c96ce0caceae74e182fcaa0781b001eda
bacnv <bacnguyen87@gmail.com>
2015-08-04 17:59:02 +0700

    Props

--- src/main/scala/code/common/Utils.scala
+++ src/main/scala/code/common/Utils.scala
@@ -5,6 +5,8 @@ import java.io.FileInputStream
 import net.liftweb.common.Full
 import net.liftweb.util.Props
 
+import scala.io.Source
+
 /**
  * Created by bacnv on 31/07/2015.
  */
@@ -12,15 +14,16 @@ object Utils {
 
   def propsWheretoLook(filename:String) : List[(String, () => Full[FileInputStream])]={
         println(Props.mode)
+   println(Props.hostName)
         Props.mode match {
-          case Props.RunModes.Test =>  (("src/main/resources/props/test/"+filename, () => Full(new FileInputStream("src/main/resources/props/test/"+filename))) :: Nil)
-          case Props.RunModes.Production =>  (("src/main/resources/props/production/"+filename, () => Full(new FileInputStream("src/main/resources/production/test/"+filename))) :: Nil)
-          case Props.RunModes.Development =>  (("src/main/resources/props/dev/"+filename, () => Full(new FileInputStream("src/main/resources/props/dev/"+filename))) :: Nil)
+          case Props.RunModes.Test =>  ((getClass.getResource("/props/test/"+filename).getPath, () => Full(new FileInputStream(getClass.getResource("/props/test/"+filename).getPath))) :: Nil)
+          case Props.RunModes.Production =>  ((getClass.getResource("/props/production/"+filename).getPath, () => Full(new FileInputStream(getClass.getResource("/props/production/"+filename).getPath))) :: Nil)
+          case Props.RunModes.Development =>  ((getClass.getResource("/props/dev/"+filename).getPath, () => Full(new FileInputStream(getClass.getResource("/props/dev/"+filename).getPath))) :: Nil)
         }
   }
   def propsWheretoLook(filename:String,any :Any) : List[(String, () => Full[FileInputStream])]={
     println(Props.mode)
-    (("src/main/resources/props/"+filename, () => Full(new FileInputStream("src/main/resources/props/"+filename))) :: Nil)
+    ((getClass.getResource("/props/"+filename).getPath, () => Full(new FileInputStream(getClass.getResource("/props/"+filename).getPath))) :: Nil)
   }
 
 }
